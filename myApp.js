05d4e4c3c69eff9b5268698839843b88d19ce02e
Shell Raiser <78999739+shell-raiser@users.noreply.github.com>
2022-07-25 02:40:35 +0000

    Configure helmet.hsts() to use HTTPS for the next 90 days

--- myApp.js
+++ myApp.js
@@ -8,6 +8,9 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+ninetyDaysInSeconds = 90*24*60*60
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+
 
 
 
